105f459858a7819c82151027678d2455d32be6e4
WaterLord404 <davidgomezrodriguezinfor@gmail.com>
2021-04-21 17:16:29 +0200

    product ahora tiene rating

--- src/app/modules/product/Interfaces/productI.ts
+++ src/app/modules/product/Interfaces/productI.ts
@@ -9,6 +9,7 @@ export interface ProductI {
   salePrice: number;
   priceWoutDiscount?: number;
   totalPrice?: number;
+  stars?: number;
   discount?: DiscountI;
   size: Array<string>;
   colour: Array<string>;
