8e9da0f9716763898567b2a07cb9da10c3a1d287
WaterLord404 <55837653+WaterLord404@users.noreply.github.com>
2021-06-18 02:41:12 +0200

    gh pages install

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  domain: 'https://cyberninja.herokuapp.com/',
+  googleClientID: '899814992698-rfm638ehb7fb39t7t52c6j44du35fg8h.apps.googleusercontent.com',
+  paypalClientID: 'AdTFRw1qz6oh1Ac8nZVQ1-668Q3vOErq0trFh813f3XHb2gfIsEiheGvQm8dKqtEv8wwU4zD3ej9VwSN'
 };
